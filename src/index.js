5c442dc542f53d9b0504232c07f56f5d341cfa0c
H-b-IO-T-O-H <importantv@yandex.ru>
2021-02-04 01:24:50 +0300

    add simple page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+//import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './pages/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import App from "./pages/App";
+import "./css/main.css";
+window.jQuery = window.$ = $;
+require('bootstrap');
+
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,16 @@
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Work!
-    </div>
-  );
+
+export class App extends Component {
+
+    render() {
+        return (
+            <div className="App h-100 d-flex align-items-center justify-content-center text-center">
+                <div className="Auth">
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
