4162c6fe9e35a69b0e5b1891134a937fc76ef83a
H-b-IO-T-O-H <importantv@yandex.ru>
2021-02-04 16:22:51 +0300

    rm jquery, bootstrap.js, install fontawesome

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@
 //import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
 import App from "./pages/App";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/main.css";
-window.jQuery = window.$ = $;
-require('bootstrap');
+
 
 
 ReactDOM.render(
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCoffee} from '@fortawesome/free-solid-svg-icons'
 
 
 export class App extends Component {
@@ -7,6 +9,7 @@ export class App extends Component {
         return (
             <div className="App h-100 d-flex align-items-center justify-content-center text-center">
                 <div className="Auth">
+                    <FontAwesomeIcon icon={faCoffee}/>
                 </div>
             </div>
         )
